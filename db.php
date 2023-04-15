6bd07a3d570ee454ecde60cd97e46ddf9acc569d
vallamkonda0 <128234858+vallamkonda0@users.noreply.github.com>
2023-04-15 09:44:11 -0500

    update

--- db.php
+++ db.php
@@ -1,6 +1,10 @@
 <?php
-
-    $con = mysqli_connect("127.0.0.1","root","","crs");
+    $host = "us-cdbr-east-06.cleardb.net";
+    $user = "b4f8f97a12a570";
+    $pass = "e1692e62";
+    $name = "heroku_7af4034455cfe47";
+    #$con = mysqli_connect("127.0.0.1","root","","crs");
+    $con = mysqli_connect($host, $user, $pass, $name);
     if (mysqli_connect_errno()){
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }
